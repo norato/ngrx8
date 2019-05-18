bd34dbe2862e5f661fd4bd70e0a9f7fc812c22ce
Felipe Norato <felipe.norato@wavy.global>
2019-05-18 13:34:32 -0300

    Use Entity

--- src/app/store/user/user.reducer.ts
+++ src/app/store/user/user.reducer.ts
@@ -1,19 +1,16 @@
 import { createReducer, on } from '@ngrx/store';
+import { EntityState, createEntityAdapter } from '@ngrx/entity';
 import { UserActions } from './user.actions';
 import { User } from './user.model';
 
-export interface UserState {
-  users: User[];
-}
+export interface UserState extends EntityState<User> {}
+const adapter = createEntityAdapter<User>();
 
-export const initialUserState: UserState = {
-  users: []
-};
+export const initialUserState: UserState = adapter.getInitialState();
 
 export const UserReducer = createReducer(
   initialUserState,
-  on(UserActions.getAllSuccess, (state, action) => ({
-    ...state,
-    users: action.payload
-  }))
+  on(UserActions.getAllSuccess, (state, action) =>
+    adapter.addAll(action.payload, state)
+  )
 );
--- src/app/store/user/user.selectors.ts
+++ src/app/store/user/user.selectors.ts
@@ -1,9 +1,9 @@
-import { UserState } from './user.reducer';
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { UserState } from './user.reducer';
 
 const getUserState = createFeatureSelector('user');
 
 export const getUsers = createSelector(
   getUserState,
-  (state: UserState) => state.users
+  (state: UserState) => Object.values(state.entities)
 );
